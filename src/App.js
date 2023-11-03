f7ebfb212e64da4fe8f54150aa12c6867ba104b6
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-11-03 04:54:39 +0000

    urlパラメーター

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
-// react-router-domから「BrowserRouter, Link, Switch, Route」をインポート
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+("react-router-dom");
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
-    // ルーティングは<BrowserRouter> で囲む
-    <BrowserRouter> 
+    <BrowserRouter>
       <div className="App">
-        {/* <Link>はaタグのようなもの。to=にパスを書く */}
         <Link to="/">Home</Link>
         <br />
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-
-      {/* <Switch>で囲むことで子コンポーネントを親コンポーネントに呼び出すことができる（=ルーティングすることができる） */}
-      {/* Home */}
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        {/* Page1 */}
-        <Route
-          path="/page1"
-          // rederでネストされたページのルーティングも可能。
-          //{ match: { url } }は現在のページのobject>url（つまりネストの親を返す）ため、ネストされたページの子ページにも指定することで、手入力で毎回page1/のように書かなくて済む！
-          render={({ match: { url } }) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-
-        {/* Page2 */}
-        <Route path="/page2" render={() => <Page2 />}>
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,52 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      {/* Page1 */}
+      <Route
+        path="/page1"
+        // renderでネストされたページのルーティングも可能。
+        //{ match: { url } }は現在のページのobject>url（つまりネストの親を返す）ため、ネストされたページの子ページにも指定することで、手入力で毎回page1/のように書かなくて済む！
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+
+      {/* Page2 */}
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.url}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+    </Switch>
+  );
+};
